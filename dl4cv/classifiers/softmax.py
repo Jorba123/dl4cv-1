d97c3ff73d469a46101252e1e2cd9674efe5b4db
Felix Schober <felix.schober@tum.de>
2017-11-07 14:06:30 +0100

    [EX1] Implement Softmax + Cross Entropy Loss

--- dl4cv/classifiers/softmax.py
+++ dl4cv/classifiers/softmax.py
@@ -30,12 +30,79 @@ def softmax_loss_naive(W, X, y, reg):
     # regularization!                                                           #
     #############################################################################
 
+    num_classes = W.shape[1]
+    num_samples = X.shape[0]
+    D = X.shape[1]
+    # compute scores based on W and X ("forward" pass)
+    f = np.zeros(num_classes)
+    losses = np.zeros(num_samples)
+    # softmax predictions
+    #y_hat = np.zeros((num_samples, num_classes))
+
+    # transform y to the same format (one-hot). Strictly speaking this contains np loops but since it just makes the
+    # calculation easier this should be fine
+    #y_oh = np.zeros((num_samples, num_classes))
+    #y_oh[np.arange(num_samples), y] = 1
+    #print('One Hot y: ', y_oh[0])
+    #print('y: ', y[0])
+    print('Num Classes ' + str(num_classes))
+
+    # do the calculation for each sample in the batch
+    for sample_index in range(num_samples):
+        sample = X[sample_index]
+        prediction = np.zeros(num_classes)
+        # iterate over classes and do matrix multiplication with explicit loops
+        for i in range(num_classes):
+            # get part of weight vector that corresponds to the i-th class. This will be a vector with shape (
+            class_weight_vector = W[:, i]
+            # iterate over pixels
+            for j in range(D):
+                prediction[i] = sample[j] * class_weight_vector[j]
+
+            f[i] += prediction[i]
+
+        # calculate softmax to get y_hat (predicted class labels)
+        y_hat = calculate_softmax(prediction)
+        assert sum(y_hat) - 1.0 < 0.001, 'Sum is not 1.0 {0}'.format(sum(y_hat))
+        # calculate cross entropy loss
+        # y_sample contains the index of the prediction value we need for the cross entropy loss calculation
+        y_sample = y[sample_index]
+        y_loss = np.log(y_hat[y_sample]) * (-1)
+        losses[sample_index] = y_loss
+
+    # calculate mean cross entropy loss
+    sum_of_losses = 0
+    for l_j in losses:
+        sum_of_losses += l_j
+    loss = sum_of_losses / num_samples
+
     #############################################################################
     #                          END OF YOUR CODE                                 #
     #############################################################################
 
     return loss, dW
 
+
+def calculate_softmax(f):
+    num_classes = f.shape[0]
+
+    # calculate softmax and cross entropy loss for the softmax
+    # to prevent numeric instability due to large numbers from exponents, normalize so that max is 0
+    max_score = np.max(f)
+    for i in range(num_classes):
+        f[i] -= max_score
+
+    # calculate divisor (sum(exp(f))
+    sum_of_scores = 0
+    for f_i in f:
+        sum_of_scores += np.exp(f_i)
+
+    softmax = np.zeros(num_classes)
+
+    for j in range(num_classes):
+        softmax[j] = np.exp(f[j]) / sum_of_scores
+
+    return softmax
 
 def softmax_loss_vectorized(W, X, y, reg):
     """
